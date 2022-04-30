727be3ecffba944fbd4210da7f7dbad58e22c373
nodchip <nodchip@gmail.com>
2022-04-30 09:53:47 +0900

    保存間隔を調整した

--- source/tanuki_book.cpp
+++ source/tanuki_book.cpp
@@ -63,7 +63,7 @@ namespace {
 	constexpr const char* kBookUctMaxSearchPerPosition = "BookUctMaxSearchPerPosition";
 	constexpr const char* kBookUctRecordFile = "BookUctRecordFile";
 	constexpr int kShowProgressPerAtMostSec = 1 * 60 * 60;	// 1時間
-	constexpr time_t kSavePerAtMostSec = 3 * 60 * 60;		// 3時間
+	constexpr time_t kSavePerAtMostSec = 1 * 60 * 60;		// 1時間
 
 	struct SfenAndMove {
 		std::string sfen;
