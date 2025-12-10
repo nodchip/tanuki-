7f3f289e220e6211cea04fd802872b4bda377e82
nodchip <nodchip@gmail.com>
2025-12-10 16:34:36 +0900

    誤ってrevertしていたところをreapplyした。

--- source/timeman.cpp
+++ source/timeman.cpp
@@ -152,6 +152,8 @@ void TimeManagement::init_(const Search::LimitsType& limits,
 	// byoyomiとincの指定は残り時間にこの時点で加算して考える。
     remain_time =
       limits.time[us] + limits.byoyomi[us] + limits.inc[us] - (TimePoint) options["NetworkDelay2"];
+	// ここを0にすると時間切れのあと自爆するのでとりあえず100はあることにしておく。
+    remain_time = std::max(remain_time, (TimePoint) 100);
 
 	// remain_timeを0にすると時間切れのあと自爆するのでとりあえず100はあることにしておく。
 	// round_up_to_fullsecond == falseのときは秒未満での戦いなので、1にしておく。
