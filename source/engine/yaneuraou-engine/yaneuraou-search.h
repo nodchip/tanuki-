fe68a2da4a6396be562859c4af110156f1e1808a
KazApps <kazuki_shogi@outlook.jp>
2026-03-18 23:14:28 +0900

    remove nmp verification search

--- source/engine/yaneuraou-engine/yaneuraou-search.h
+++ source/engine/yaneuraou-engine/yaneuraou-search.h
@@ -529,7 +529,7 @@ class YaneuraOuWorker: public Worker {
 
     // selDepth : 選択探索の深さ。
     // 💡depthとPV lineに対するUSI infoで出力するselDepth。
-    int selDepth, nmpMinPly;
+    int selDepth;
 
 	// 探索時に評価値に楽観的バイアスを与えるために用いるパラメーター。
 	Value optimism[COLOR_NB];
