de196df4298c257c6939a7ce9d56cc6824cf165a
nodchip <nodchip@gmail.com>
2026-03-21 12:17:23 +0900

    Fix AVX512 NNUE accumulator chunk count

--- source/eval/nnue/nnue_feature_transformer.h
+++ source/eval/nnue/nnue_feature_transformer.h
@@ -489,7 +489,7 @@ class FeatureTransformer {
 					auto accumulation      = reinterpret_cast<vec_t*>(&accumulator.accumulation[perspective][i][0]);
 					auto column            = reinterpret_cast<const vec_t*>(&weights_[offset]);
 #if defined(USE_AVX512)
-					constexpr IndexType kNumChunks = kHalfDimensions / kSimdWidth;
+					constexpr IndexType kNumChunks = kHalfDimensions / (kSimdWidth / 2);
 #else
 					constexpr IndexType kNumChunks = kHalfDimensions / (kSimdWidth / 2);
 #endif
@@ -531,7 +531,7 @@ class FeatureTransformer {
 			for (Color perspective : {BLACK, WHITE}) {
 #if defined(VECTOR)
 #if defined(USE_AVX512)
-				constexpr IndexType kNumChunks = kHalfDimensions / kSimdWidth;
+				constexpr IndexType kNumChunks = kHalfDimensions / (kSimdWidth / 2);
 #else
 				constexpr IndexType kNumChunks = kHalfDimensions / (kSimdWidth / 2);
 #endif
