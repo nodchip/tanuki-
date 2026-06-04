98c11568374efe97513bdccd8f966da5b39ff6ae
nodchip <nodchip@gmail.com>
2026-06-04 19:00:12 +0900

    ネットワークアーキテクチャーを SFNN_halfka2_1024_7_32_p に変更した。

--- source/eval/nnue/architectures/sfnnwop-1536.h
+++ source/eval/nnue/architectures/sfnnwop-1536.h
@@ -23,7 +23,7 @@ using RawFeatures = Features::FeatureSet<
 
 // Number of input feature dimensions after conversion
 // 変換後の入力特徴量の次元数
-constexpr IndexType kTransformedFeatureDimensions = 1536;
+constexpr IndexType kTransformedFeatureDimensions = 1024;
 
 // Number of networks stored in the evaluation file
 constexpr int LayerStacks = 8;
